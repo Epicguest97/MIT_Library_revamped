df5e0408dc45b67bb776622e8d480a1f63093856
Epicguest97 <kaushikmehul94@gmail.com>
2026-09-22 23:51:27 +0530

    2025

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-background py-8">
       <div className="max-w-5xl mx-auto mb-6 p-3 bg-neutral-800 text-white rounded text-sm text-center">
-        MIT has finally taken steps to significantly improve its library portal ...a lot better actually, so I’ve decided to deprecate my own site. I’m really glad to see the official platform getting the attention it needed. <a href="https://mitmpllibportal.manipal.edu/question-papers">view here</a>
+        2025 question papers have been added. See the new library site here - <a href="https://mitmpllibportal.manipal.edu/question-papers">view here</a>
       </div>
       <LinkFinder />
       <Analytics />
